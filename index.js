f494a983add28cdaea5c4bcfdec3f19b32169476
chamostar3@gmail.com <chamostar3@gmail.com>
2023-07-05 17:51:39 -0500

    configuracion de servidor manual

---
+++ index.js
@@ -0,0 +1,8 @@
+const http = require ("http")
+const { CLIENT_RENEG_LIMIT } = require("tls")
+function requestController(){
+   console.log("recibimos una nueva request")
+}
+//configurar nuestro servidor
+const server =http.createServer(requestController)
+server.listen(4000)
