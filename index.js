7ebb75aa7de1657a6073f8f356ec199dbd85b234
chamostar3@gmail.com <chamostar3@gmail.com>
2023-07-08 15:07:09 -0500

    errores corregidos, listo para subir a github ejemplo js puro

--- index.js
+++ index.js
@@ -1,7 +1,33 @@
 require ("dotenv").config()
+const fs = require("fs")
 const http = require ("http")
-function requestController(){
-   console.log("HOLA MUNDO")
+function requestController(req, res){
+   const url = req.url
+   const method = req.method
+   
+   if (method === "GET" && url === "/") {
+      res.setHeader("Content-Type", "text/html; charset=UTF-8")
+      fs.readFile('./public/index.html', function (err,file){
+         if (err) {
+            console.log("hubo un error")
+         }
+         res.write(file)
+         res.end()
+      })
+            return
+   }
+   if (method === "GET" && url === "/about") {
+      console.log({url, method})
+      res.setHeader("Content-Type", "text/html; charset=UTF-8")
+      fs.readFile("./public/about.html", function (err , file){
+         if (err) {
+            console.log("hubo un error")
+         }
+         res.write(file)
+         res.end()
+      })
+            return
+   }
 }
 //configurar nuestro servidor
 const server = http.createServer(requestController)
